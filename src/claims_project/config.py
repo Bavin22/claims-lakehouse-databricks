aec29915a3c3d52cd5e1c5a5accef1c87850a393
Bavin22 <bavinaadithiya@gmail.com>
2026-06-01 15:50:07 +0000

    config file(project details)

---
+++ src/claims_project/config.py
@@ -0,0 +1,15 @@
+CATALOG = "dev"
+SCHEMA = "claims_project"
+
+RAW_PATH = "/Volumes/dev/claims_project/raw"
+CHECKPOINT_PATH = "/Volumes/dev/claims_project/checkpoints"
+
+TABLES = {
+    "bronze_claims": "dev.claims_project.bronze_claims",
+    "bronze_providers": "dev.claims_project.bronze_providers",
+    "silver_claims": "dev.claims_project.silver_claims",
+    "silver_providers": "dev.claims_project.silver_providers",
+    "gold_provider_monthly": "dev.claims_project.gold_provider_monthly",
+    "gold_claim_lag": "dev.claims_project.gold_claim_lag",
+    "gold_suspicious_claims": "dev.claims_project.gold_suspicious_claims",
+}
