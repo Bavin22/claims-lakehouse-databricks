e826299b6de2dcafcf4d1c66d6df7679e26df489
Bavin22 <bavinaadithiya@gmail.com>
2026-06-02 14:32:21 +0000

    added enriched silver table's name to the dict

--- src/claims_project/config.py
+++ src/claims_project/config.py
@@ -9,6 +9,7 @@ TABLES = {
     "bronze_providers": "dev.claims_project.bronze_providers",
     "silver_claims": "dev.claims_project.silver_claims",
     "silver_providers": "dev.claims_project.silver_providers",
+    "silver_claims_enriched": "dev.claims_project.silver_claims_enriched",
     "gold_provider_monthly": "dev.claims_project.gold_provider_monthly",
     "gold_claim_lag": "dev.claims_project.gold_claim_lag",
     "gold_suspicious_claims": "dev.claims_project.gold_suspicious_claims",
